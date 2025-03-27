e94111a454b43408061d782c368a88265e908a07
524hhhhhh <srose1225@naver.com>
2025-03-28 04:36:43 +0900

    :lipstick:design: upate lnput component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import { cn } from '@lib/utils';
 
 function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
@@ -8,9 +7,31 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot="input"
       className={cn(
-        'border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+        // 텍스트 & 배경
+        'text-sm text-[var(--text-primary)]',
+        'bg-[var(--fill-quaternary)]',
+
+        // 패딩
+        'pl-3 py-[17px]',
+
+        // 레이아웃 & 모양
+        'flex w-full min-w-0 h-auto rounded-md',
+        'shadow-xs transition-[color,box-shadow] outline-none',
+
+        // border
+        'border border-transparent focus:border-[var(--fill-primary)]',
+
+        // placeholder & selection
+        'placeholder:text-[var(--text-tertiary)] selection:bg-primary selection:text-primary-foreground',
+
+        // 파일 인풋
+        'file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium',
+
+        // disabled & aria 상태
+        'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        'aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 aria-invalid:ring-destructive/20',
+
+        // 커스텀 클래스
         className,
       )}
       {...props}
