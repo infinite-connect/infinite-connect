649bb30527a8d48c38ee454709bc4f7d3b7cce26
524hhhhhh <srose1225@naver.com>
2025-03-28 09:44:08 +0900

    :lipstick:design: Fix background image and text position

--- src/components/SignupPage/Success.tsx
+++ src/components/SignupPage/Success.tsx
@@ -8,43 +8,45 @@ const Success = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative  bg-[#0C0C0D] font-[NanumGothic]">
-      <Header className="pl-[4px]">
+    <div className="flex flex-col min-h-screen bg-[#0C0C0D] font-[NanumGothic]  ">
+      {/* 헤더 */}
+      <Header className="pl-[20px]">
         <Header.Left>
           <Logo />
           <div> Sign up</div>
         </Header.Left>
       </Header>
+
+      {/* 본문 */}
       <div
-        className="flex flex-col  min-h-screen px-6 pt-20 pb-10 bg-no-repeat bg-cover bg-center"
-        style={{ backgroundImage: `url(${SuccessBg})`, backgroundSize: '100% auto' }}
+        className="flex flex-col flex-1 bg-no-repeat bg-cover bg-center px-6"
+        style={{
+          backgroundImage: `url(${SuccessBg})`,
+          backgroundSize: '100% auto',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center -40px',
+        }}
       >
-        <div className="flex-1 flex flex-col justify-center items-center  text-center text-[var(--text-primary)]">
-          {/* 상단 text */}
-          <div>
-            <h1 className="text-[28px] font-semibold leading-tight">
-              에잇님
-              <br />
-              환영합니다
-            </h1>
-          </div>
-          {/* 하단 text */}
-          <div className="mt-[105px]">
-            <p className="text-sm leading-relaxed text-white/80 mb-10">
-              에잇님의 명함을 공유하고 컨퍼런스 참석자들과
-              <br />
-              즐거운 네트워킹 시간을 보내보세요!
-            </p>
-          </div>
+        {/* 중앙 텍스트 영역 */}
+        <div className="flex flex-1 flex-col items-center justify-center text-center text-[var(--text-primary)]">
+          <h1 className="text-[28px] font-semibold leading-tight mb-6">
+            에잇님
+            <br />
+            환영합니다
+          </h1>
+          <p className="mt-[135px] text-[14px] text-sm leading-relaxed text-white/80">
+            에잇님의 명함을 공유하고 컨퍼런스 참석자들과
+            <br />
+            즐거운 네트워킹 시간을 보내보세요!
+          </p>
         </div>
 
-        {/* button */}
-        <Button
-          className="w-full max-w-sm mx-auto h-[52px] mt-[120px]"
-          onClick={() => navigate('/selectcarddesign')}
-        >
-          나의 네트워킹 성향 선택하기
-        </Button>
+        {/* 하단 버튼 영역 */}
+        <div className="w-full max-w-sm mx-auto pb-10">
+          <Button className="w-full h-[52px]" onClick={() => navigate('/selectcarddesign')}>
+            나의 네트워킹 성향 선택하기
+          </Button>
+        </div>
       </div>
     </div>
   );
