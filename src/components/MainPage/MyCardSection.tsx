b524abadb5f547ab86443e8fd078d0bf0acd8a9b
Hyun Sang <pycandyq@naver.com>
2025-03-29 01:41:44 +0900

    ✨feat: Add vertical business card and circular progress

--- src/components/MainPage/MyCardSection.tsx
+++ src/components/MainPage/MyCardSection.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Slider from 'react-slick';
-import moringCard from '@assets/NetWorkTypeCard/morningCard.png';
+import VerticalCardPreview from '@components/commons/Card/VerticalCardPreview';
+import { useSelector } from 'react-redux';
+import { RootState } from '@store/store';
+import CircleProgress from './CircleProgres';
+
 const MyCardSection = (): React.JSX.Element => {
+  const primaryCard = useSelector((state: RootState) => state.userBusinessCard.primaryCard);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -11,20 +17,20 @@ const MyCardSection = (): React.JSX.Element => {
     arrows: false,
   };
   return (
-    <div className="mx-4 px-8 py-4 flex flex-col bg-[#1E1E1E] rounded-md">
+    <div className="mx-4 px-4 py-4 flex flex-col bg-[#1E1E1E] rounded-md">
       <h2 className="text-center text-[16px] font-[NanumGothicOTF] font-semibold">
         즐거운 저녁의 커넥터
       </h2>
-      <h3 className="text-center text-[12px] font-[NanumGothicOTF] font-normal text-[var(--text-secondary)] mb-4">
+      <h3 className="text-center text-[12px] font-[NanumGothicOTF] font-normal text-[var(--text-secondary)]">
         추가 정보를 작성하면 사람들과 더 쉽게 연결돼요
       </h3>
       <div className="">
         <Slider {...settings}>
           <div className="flex flex-col">
             <div className="flex flex-row gap-4">
-              <img className="w-[175px]" src={moringCard} />
-              <div className="w-full flex flex-col justify-between">
-                <div>동그라미</div>
+              <VerticalCardPreview cardId={primaryCard?.business_card_id || ''} />
+              <div className="w-full flex flex-col justify-between ali">
+                <CircleProgress />
                 <div className="bg-[rgba(255,255,255,0.05)] px-2 py-3 gap-2 flex flex-col">
                   <div className="flex justify-between">
                     <p className="text-[12px] text-[var(--text-secondary)] ">저장된 내 명함</p>
@@ -47,15 +53,6 @@ const MyCardSection = (): React.JSX.Element => {
           </div>
           <div>
             <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
           </div>
         </Slider>
       </div>
